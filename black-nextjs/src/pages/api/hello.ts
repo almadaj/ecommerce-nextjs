a17ab31662920568cd4e73fab690654c9dd9735c
Juliano Almada <almadaj@edu.unifor.br>
2023-11-12 16:34:15 -0300

    criando server side e client side

--- black-nextjs/src/pages/api/hello.ts
+++ black-nextjs/src/pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+  timestamp: Date;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const timestamp = new Date();
+  res.status(200).json({ name: "John Doe", timestamp });
 }
---
+++ black-nextjs/src/pages/dynamic.tsx
@@ -0,0 +1,56 @@
+import { GetServerSideProps, NextPage } from "next";
+import { ReactNode, useEffect, useState } from "react";
+import { Col, Container, Row } from "reactstrap";
+
+type ApiResponse = {
+  name: string;
+  timestamp: Date;
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const serverSideData: ApiResponse = await fetch(
+    `http://localhost:3000/api/hello`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      serverSideData,
+    },
+  };
+};
+
+const Dynamic: NextPage = (props: {
+  children?: ReactNode;
+  serverSideData?: ApiResponse;
+}) => {
+  const [clientSideData, setClientSideData] = useState<ApiResponse>();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("/api/hello").then((res) => res.json());
+    setClientSideData(data);
+  };
+
+  return (
+    <Container tag="main">
+      <h1 className="my-5">Como funcionam as renderizações do Next.js</h1>
+
+      <Row>
+        <Col>
+          <h3>
+            Gerado no servidor: {props.serverSideData?.timestamp.toString()}
+          </h3>
+        </Col>
+
+        <Col>
+          <h3>Gerado no cliente: {clientSideData?.timestamp.toString()}</h3>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Dynamic;
