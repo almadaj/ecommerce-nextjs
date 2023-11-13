561e1ae2535961805e83bc42117717db8397376d
Juliano Almada <almadaj@edu.unifor.br>
2023-11-13 15:23:35 -0300

    API de products criada

---
+++ black-nextjs/src/pages/api/products/[id].ts
@@ -0,0 +1,11 @@
+// pages/api/products/[id].ts
+
+import { NextApiRequest, NextApiResponse } from "next";
+import products from "../../../../database.json";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
+
+  const product = products.find((p) => p.id === Number(id));
+  res.status(200).json(product);
+}
